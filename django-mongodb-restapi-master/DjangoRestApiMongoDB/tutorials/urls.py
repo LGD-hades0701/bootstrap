dad40243ab8bafa01d6cf56d6904cdcebf2f53d8
dashdev0129 <122053519+dashdev0129@users.noreply.github.com>
2023-02-15 11:32:58 -0500

    Delete django-mongodb-restapi-master directory

--- django-mongodb-restapi-master/DjangoRestApiMongoDB/tutorials/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url 
-from tutorials import views 
- 
-urlpatterns = [ 
-    url(r'^api/tutorials$', views.tutorial_list),
-    url(r'^api/tutorials/(?P<pk>[0-9]+)$', views.tutorial_detail),
-    url(r'^api/tutorials/published$', views.tutorial_list_published)
-]
